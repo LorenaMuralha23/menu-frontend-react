378d1edd48001e9356b2f9890e7f93f5e021e49d
Lorena Muralha <mlmuralha2301@gmail.com>
2024-04-25 14:51:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FoodData } from './interface/FoodData';
+import { Card } from './components/card/card';
+
+function App() {
+  const data: FoodData[] = [];
+  return (
+    <div className="container">
+      <h1>Menu</h1>
+      <div className='card-grid'>
+        {data.map(foodData => <Card
+          price={foodData.price}
+          title={foodData.title}
+          image={foodData.image}
+        />)}
+      </div>
+    </div>
+  )
+}
+
+export default App
